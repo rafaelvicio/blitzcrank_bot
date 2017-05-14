6709e484aef8c6e218ca7c3a675cc9a4b871e821
Rafael Vicio <rafaelvicio@icloud.com>
2017-05-14 01:59:46 -0300

    Commandos basicos

--- index.js
+++ index.js
@@ -2,27 +2,52 @@ const TelegramBot = require( `node-telegram-bot-api` )
 const http = require( `axios` )
 
 const token = `385522563:AAFpYSShk-aSj6dg6pMPm4NY4rXTuXef17o`
-const url = `https://br1.api.riotgames.com/lol/summoner/v3/summoners/by-name/`
 const key = `?api_key=RGAPI-a1503db2-d279-47cb-9f38-e73da6d734fc`
+const url_id = `https://br1.api.riotgames.com/lol/summoner/v3/summoners/by-name/`
+const url_level = `https://br1.api.riotgames.com/lol/summoner/v3/summoners/`
+const url_status = `https://br1.api.riotgames.com/lol/status/v3/shard-data`
+const url_ranked = `https://br1.api.riotgames.com/lol/league/v3/positions/by-summoner/`
+const url_challenger = `https://br1.api.riotgames.com/lol/league/v3/challengerleagues/by-queue/RANKED_SOLO_5x5`
 
 const bot = new TelegramBot( token, { polling: true } )
 
-const logErrorEcho = ( msg ) => ( err ) =>
-  console.log( msg, err )
+  const cb1 = (response) =>
+  http.get(`${url_level}${response.data.id}${key}`)
 
-const logSuccessEcho = ( msg, match ) => ( data ) =>
-  console.log( `Success: `, data )
+  const cb2 = (response) =>
+  http.get(`${url_status}${response.data.id}${key}`)
 
-const sendStart = ( msg, match ) =>
-  bot.sendMessage( msg.chat.id, 'Olá, ' + msg.chat.first_name + ' eu sou o Bot Calouro, digite /help para conhecer o que eu posso fazer por voce! ;)')
-    .then( logSuccessEcho( msg, match ) )
-    .catch( logErrorEcho( `Error: ` ) )
+  const cb3 = (response) =>
+  http.get(`${url_ranked}${response.data.id}${key}`)
 
-const sendInvocador = ( msg, match ) =>
-    http.get(`${url}${match[ 1 ]}${key}`)
-      .then((response) => bot.sendMessage(msg.chat.id, response.data.name))
-      .catch( logErrorEcho( `Error: ` ) )
+  const cb4 = (response) =>
+  http.get(`${url_challenger}${response.data.id}${key}`)
 
-bot.onText( /\/invocador (.*)/, sendInvocador)
+  const sendLevel = ( msg, match ) =>
+  http.get(`${url_id}${match[ 1 ]}${key}`)
+    .then( cb1 )
+    .then((response) => console.log(response.data.summonerLevel))
+    .catch((err) => console.log(err))
 
-bot.onText( /\/start(.*)/, sendStart)
+  const sendStatus = ( msg, match ) =>
+  http.get(`${url_id}${match[ 1 ]}${key}`)
+    .then( cb2 )
+    .then((response) => console.log(response.data))
+    .catch((err) => console.log(err))
+
+  const sendRanked = ( msg, match ) =>
+  http.get(`${url_id}${match[ 1 ]}${key}`)
+    .then( cb3 )
+    .then((response) => console.log(response.data))
+    .catch((err) => console.log(err))
+
+  const sendChallenger = ( msg, match ) =>
+  http.get(`${url_id}${match[ 1 ]}${key}`)
+    .then( cb4 )
+    .then((response) => console.log(response.data))
+    .catch((err) => console.log(err))
+
+bot.onText( /\/invocador (.*)/, sendLevel)
+bot.onText( /\/status (.*)/, sendStatus)
+bot.onText( /\/ranked (.*)/, sendRanked)
+bot.onText( /\/challenger (.*)/, sendChallenger)
