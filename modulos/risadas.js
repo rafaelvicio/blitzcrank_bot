8069810d92ef9e25c806146afdbc399636fbe38c
Rafael Vicio <rafaelvicio@icloud.com>
2017-05-21 02:40:00 -0300

    Varias risadas

--- modulos/risadas.js
+++ modulos/risadas.js
@@ -2,8 +2,15 @@ const http = require( `axios` )
 
 const bot = require('../config/bot')
 
+const risadas = [
+  'hehehehehe',
+  'hahahahaha',
+  'hauhauhauh',
+  'kkkkkkkkkk'
+]
+
 const sendRisadas = ( msg, match ) =>
-  bot.sendMessage( msg.chat.id, "hahahaha" )
+  bot.sendMessage( msg.chat.id, risadas[Math.floor(Math.random() * risadas.length)] )
     .then( logSuccessEcho( msg, match ) )
     .catch( logErrorEcho( 'Error:') );
 
