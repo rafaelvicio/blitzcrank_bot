a284f391772ced513732009d278d2e0be5f902f7
Rafael Vicio <rafaelvicio@icloud.com>
2017-05-22 22:56:58 -0300

    v0.3

--- config/bot.js
+++ config/bot.js
@@ -1,8 +1,8 @@
 const TelegramBot = require( `node-telegram-bot-api` )
 
 const token_teste = `318394911:AAE6OsdVjWyPTxJ1X0Vcf2lzYEeJxPBq52c` // Bot Calouro
-const token_prod = `318394911:AAE6OsdVjWyPTxJ1X0Vcf2lzYEeJxPBq52c` // Bot Produção
+const token_prod = `385522563:AAFpYSShk-aSj6dg6pMPm4NY4rXTuXef17o` // Bot Produção
 
-const bot = new TelegramBot( token_teste, { polling: true } )
+const bot = new TelegramBot( token_prod, { polling: true } )
 
 module.exports = bot
